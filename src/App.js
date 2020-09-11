3bd15e36ac41cd465b829324e129cf943f923eba
Sam Tarullo <sam@Sams-Air.attlocal.net>
2020-09-11 11:23:19 -0500

    trouble

--- src/App.js
+++ src/App.js
@@ -23,8 +23,8 @@ console.log(characters);
   return (
     <div className="App">
       <h1 className="Header">Characters</h1>
-      {characters.map(char=>{
-        return <Character character={char} />
+      {characters.map((char)=>{
+        return <Character  />
       })}
     </div>
   );
